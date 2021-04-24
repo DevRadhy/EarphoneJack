7aeec3b62247b57de41d15d4fcdf5faf8362d48a
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2021-04-24 01:18:17 -0300

    Adicionando MessageEmbed

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const ytdl = require('ytdl-core');
 const { playlist } = require('../controllers/MusicController');
 const { seachVideos } = require('../utils/YoutubeUtils');
@@ -9,15 +10,28 @@ module.exports = async (client, message, args, music) => {
 
   if(!ytdl.validateURL(args[0])) {
     const argsQuery = message.content.slice(9).trim();
-    
+
     const videos = await seachVideos(argsQuery);
+
+    const embed = new MessageEmbed()
+
+    embed.setColor('#ffd596')
+    embed.setTitle('Escolha uma música.')
+    embed.setDescription('Escolha um número de 1-10')
+
+    videos.map((song, index) => {
+      embed.addField(`${index + 1}. ${song.title}`, song.title);
+    });
+
+    embed.setFooter(`Requested by ${message.author.tag}`, message.author.avatarURL())
+    embed.setTimestamp()
+
+    message.channel.send(embed);
     
     ytdl.validateID(videos[0].video_id);
 
-    url = `https://youtube.com/watch?v=${videos[0].video_id}`
+    url = `https://youtube.com/watch?v=${videos[0].video_id}`;
   }
-
-  console.log(url)
   
   playlist.push(url);
 
--- src/commands/skip.js
+++ src/commands/skip.js
@@ -1,4 +1,14 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = async (client, message, args, music) => {
   await music.skip();
+
+  const embed = new MessageEmbed()
+
+  embed.setColor('#ffd596')
+  embed.setTitle('Música pulada!')
+  embed.setFooter(`Requested by ${message.author.tag}`, message.author.avatarURL())
+  embed.setTimestamp()
+
   return message.channel.send("A música foi pulada!");
 }
