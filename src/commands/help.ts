1797a44c1da1a567fdaf75911a2024ae40f48e0a
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-05-04 15:10:45 -0300

    Added help command

---
+++ src/commands/help.ts
@@ -0,0 +1,37 @@
+import fs from "fs";
+import { Client, MessageEmbed } from "discord.js";
+import { ICommandsProps } from "../DTO/CommandsDTO";
+
+const getClientAvatarURL = (client: Client) => {
+    const avatarUrl = client.user?.avatarURL();
+    return avatarUrl ? String(avatarUrl) : String(client.user?.defaultAvatarURL);
+};
+
+export const help = async ({ client, message }: ICommandsProps) => {
+    const embed = new MessageEmbed();
+    const clientAvatarUrl = getClientAvatarURL(client);
+
+    embed.setColor('#ffd596');
+    embed.setTitle('EarphoneHelp');
+    embed.setDescription('Comandos do EarphoneJack');
+    embed.setAuthor(client.user?.username, clientAvatarUrl);
+    embed.setThumbnail(clientAvatarUrl);
+
+    const files = fs.readdirSync(__dirname);
+    files.map((file) => {
+        const module = require(`./${file}`);
+        const { name, description, alias } = module.details;
+        embed.addField(`${name.toUpperCase()} (${alias})`, description);
+    });
+
+    embed.setFooter(`Requested by ${message.author.tag}`, String(message.author.avatarURL()));
+    embed.setTimestamp();
+
+    return message.channel.send(embed);
+};
+
+export const details = {
+    name: 'help',
+    description: 'Mostra todos os comandos do BOT.',
+    alias: ['ajuda'],
+};
