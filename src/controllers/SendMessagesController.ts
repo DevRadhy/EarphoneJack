4e577a53e9b771a725a4650cbc90288a1efccccc
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-05-28 01:43:38 -0300

    Add - pre test roles

--- src/controllers/SendMessagesController.ts
+++ src/controllers/SendMessagesController.ts
@@ -5,6 +5,7 @@ import { validationResult } from 'express-validator';
 interface IRequest {
   guildId: string
   channelId: string
+  userId: string
   message: string[] | string
   reactions: EmojiIdentifierResolvable[]
 }
@@ -28,18 +29,37 @@ class SendMessageController {
     const {
       guildId,
       channelId,
+      userId,
       message,
       reactions
     } = request.body as IRequest;
+    
+    const guild = await this.client.guilds.cache.find(guild => guild.id === guildId);
 
-    const channelSelected = this.client.channels.cache.find(channelObject =>     
-      channelObject.id === channelId
-      //@ts-ignore
-      && channelObject.guild.id === guildId
-    );
+    const channelSelected = guild?.client.channels.cache.find(channelObject => channelObject.id === channelId );
+
+    const userIsValid = guild?.client.users.cache.find(user => user.id === userId);
+
+    if(!userIsValid){
+      return response.status(401).json({
+        errors: "User is invalid!"
+      });
+    }
+
+    const user = guild?.member(userIsValid);
+
+    
+    //@ts-ignore
+    const roleIsValid = user?._roles[0] ? user._roles.reduce((current, next) => current || next) : false;
+    
+    if(!roleIsValid){
+      return response.status(401).json({
+        error: "Role is invalid!"
+      });
+    }
   
     const formatMessage = (typeof message === "string") ? message : message.join("\n");
-  
+
     //@ts-ignore
     const messageResponse = await channelSelected.send(formatMessage) as Message;
   
